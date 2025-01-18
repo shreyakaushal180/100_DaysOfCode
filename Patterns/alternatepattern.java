50dd0fae739bcbfca9b69ae6c84748eab1c4f800
Shreya Kaushal <shreyakaushal118@gmail.com>
2025-01-18 18:10:00 +0530

    pattern

---
+++ Patterns/alternatepattern.java
@@ -0,0 +1,10 @@
+public class alternatepattern {
+    public static void main(String[] args) {
+        for(int i = 1 ; i <= 4; i++){
+            if(i % 2 == 0){
+                System.out.print(" ");
+            }
+            System.out.println("****");
+        }
+    }
+}
