41e925cb1c1e56340b1a82842e6bef7c944a9297
Shreya Kaushal <shreyakaushal118@gmail.com>
2025-02-02 16:54:49 +0530

    counts total pairs

--- Arrays/pairsInArrays.java
+++ Arrays/pairsInArrays.java
@@ -2,12 +2,15 @@ package Arrays;
 
 public class pairsInArrays {
     public static void pairs_in_array(int num []){
+        int totalPairs=0;
         for(int i = 0; i < num.length ; i++ ){
             int current = num[i];
             for(int j = i+1; j<num.length; j++){
                 System.out.print("("+current+","+ num[j]+")");
+                totalPairs++;
             }
-        }
+        }System.out.println();
+        System.out.println("Total number of possible pairs: "+totalPairs);
     }
     public static void main(String[] args) {
         int num [] = {1,2,3,4,5};
