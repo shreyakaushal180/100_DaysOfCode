c62bc58292ec643ca6093abcdb1e059043a312ba
Shreya Kaushal <shreyakaushal118@gmail.com>
2025-06-01 21:28:56 +0530

    Buy ans sell stocks to obtain maximum profit

---
+++ Arrays/BuyAndSellStocks.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+
+public class BuyAndSellStocks {
+    public static int MaxProfit(int prices []) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for(int i = 0; i < prices.length; i++){
+            if(buyPrice < prices[i]){
+                int profit = prices[i] - buyPrice;
+                
+                if(profit> maxProfit){
+                    maxProfit = profit;
+                }
+               
+            }
+            if(buyPrice > prices[i]){
+                buyPrice = prices[i];
+            }
+        }return maxProfit;
+
+    } 
+        public static void main(String[] args) {
+       int prices [] = {7, 1, 5, 3, 6, 4};
+       System.out.println(MaxProfit(prices));
+
+    }
+}
